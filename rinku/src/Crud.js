e447ed0ff3e0128dd62d04bcfc865bf8b69e2213
Rinku NImaje <124992262+Rinku3107@users.noreply.github.com>
2024-02-26 05:59:42 +0000

    rinku

--- rinku/src/Crud.js
+++ rinku/src/Crud.js
@@ -1,9 +1,60 @@
 function Crud()
 {
-    return(
-        <div>
-        <h1>RInku</h1>
-        </div>
-    )
+    const[name,setName]=useState('');
+  const[Alldata,setAllData]=useState([]);
+  const[show,setShow]=useState(false)
+  const[edit,setEdit]=useState()
+
+  const AddData=()=>{
+    if(name.length!==0)
+    {
+      setAllData(newData=>[...newData,name])
+      setName("")
+    }
+  }
+
+  const handleDelete=(index)=>{
+      Alldata.splice(index,1);
+      setAllData([...Alldata]);
+  }
+
+  const handleEdit=(i)=>{
+    setName(Alldata[i]);
+    setShow(true);
+    setEdit(i);
+  }
+
+  const handleUpdate=()=>{
+    Alldata.splice(edit,1,name);
+    setAllData([...Alldata]);
+    setShow(false);
+    setName("");
+  }
+
+
+
+
+
+  return(
+      <div className="App">
+          <h1>CRUD</h1>
+          <input type="text" value={name} onChange={(e)=>setName(e.target.value)}></input>
+          {!show?<button onClick={AddData}>Add</button>:<button onClick={handleUpdate}>save</button>}
+         
+
+          {
+            Alldata.map((val,i)=>
+            <div>
+              <h1>{val}</h1>
+              <button onClick={()=>handleEdit(i)}>Edit</button>
+              <button onClick={()=>handleDelete(i)}>Delete</button>
+            </div>
+            )
+          }
+      </div>
+
+  )
+
 }
+
 export default Crud;
